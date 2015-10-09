2a7369c3436b95d324f1cd8976740735f05bfaa1
Casey Panzer <caseypanzer@Caseys-MacBook-Air.local>
2015-10-09 10:43:38 -0400

    added mongo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express  = require('express'),
     PORT     = process.env.PORT || 5432,
     server   = express(),
-    MONGOURI = process.env.MONGOLAB_URI,
+    MONGOURI = process.env.MONGOLAB_URI || 'mongodb://localhoset:27017',
     dbname   = "some_useful_name"
     mongoose = require('mongoose');
 
