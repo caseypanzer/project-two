340c15fe4279b21e14aae7aed4ed8dffbde1a0dd
Casey Panzer <caseypanzer@Caseys-MacBook-Air.local>
2015-10-09 10:39:39 -0400

    added mongo

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var express = require('express'),
-    PORT    = process.env.PORT || 5432,
-    server  = express();
+var express  = require('express'),
+    PORT     = process.env.PORT || 5432,
+    server   = express(),
+    MONGOURI = process.env.MONGOLAB_URI,
+    dbname   = "some_useful_name"
+    mongoose = require('mongoose');
 
 server.get('/test', function(req, res){
   res.write('Welcome to my amzing app');
   res.end();
 });
 
+mongoose.connect(MONGOURI + "/" + dbname)
 server.listen(PORT, function (){
   console.log("server is up on port: " + PORT);
 });
