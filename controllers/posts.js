d5cca95d9181265a1f3ab87f498991a117ce96df
Casey Panzer <caseypanzer@Caseys-Air.fios-router.home>
2015-10-12 18:29:41 -0400

    able to show individual entries

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,6 +4,17 @@ var express = require('express'),
 
 // routes
 
+//VIEW
+router.get('/welcome', function(req, res){
+  Post.find({}, function(err, postsArray){
+    if(err){
+      console.log(err);
+    } else {
+        res.render("posts/welcome", { posts: postsArray })
+    };
+  });
+});
+
 //New Post
 router.get('/new', function(req, res){
   res.render('posts/new');
@@ -19,16 +30,23 @@ router.post('/', function(req, res){
   });
 });
 
-//VIEW
-router.get('/welcome', function(req, res){
-  Post.find({}, function(err, postsArray){
+//VIEW SINGLE RECORD
+router.get('/:id', function(req, res){
+  var mongoID = req.params.id
+  Post.findOne({ _id: req.params.id }, function(err, foundPost){
     if(err){
       console.log(err);
     } else {
-        res.render("posts/welcome", { posts: postsArray })
-    }
+        res.render("posts/view", { post: foundPost })
+    };
   });
 });
+
+//EDIT
+
+
+//PATCH
+
 
 //export router object
 module.exports = router;
