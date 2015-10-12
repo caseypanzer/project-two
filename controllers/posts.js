be8a223efcf44dc40b833032467eda1fb93a8927
Casey Panzer <caseypanzer@Caseys-Air.fios-router.home>
2015-10-12 18:06:07 -0400

    posts load

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,33 @@
 var express = require('express'),
     router  = express.Router(),
-    User    = require('../models/post.js');
+    Post    = require('../models/post.js');
 
 // routes
 
 //New Post
 router.get('/new', function(req, res){
   res.render('posts/new');
+});
+
+//CREATE
+router.post('/', function(req, res){
+  var newPost = Post(req.body.post);
+  console.log(newPost);
+
+  newPost.save(function (err, post){
+      res.redirect(302,"/posts/" + post._id)
+  });
+});
+
+//VIEW
+router.get('/welcome', function(req, res){
+  Post.find({}, function(err, postsArray){
+    if(err){
+      console.log(err);
+    } else {
+        res.render("posts/welcome", { posts: postsArray })
+    }
+  });
 });
 
 //export router object
--- models/post.js
+++ models/post.js
@@ -2,12 +2,13 @@ var mongoose = require ('mongoose'),
     Schema   = mongoose.Schema;
 
 var postSchema = new Schema({
-  lastUpdated: {type: String, required: true},
-  lastUpdatedBy: {type: String, required: true},
+  lastUpdated: { type: String },
+  lastUpdatedBy: { type: String},
   title: {type: String, required: true},
   articleText: {type: String, required: true},
   coverPhoto: {type: Array},
-  previousEntries: {type: Array}
+  previousEntries: {type: Array},
+  category_ID: {type: String}
 });
 
 var Post = mongoose.model("Post", postSchema);
