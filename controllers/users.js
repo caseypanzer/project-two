3cd10001939b998a4c77fe1e0e523f9da8e02deb
Casey Panzer <caseypanzer@Caseys-Air.fios-router.home>
2015-10-12 09:49:46 -0400

    set up server and added folders for views, models, and controllers

---
+++ controllers/users.js
@@ -0,0 +1,6 @@
+var express = require('express'),
+    router  = express.Router(),
+    User    = ('../models/user.js');
+
+//export router object
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 var express  = require('express'),
     PORT     = process.env.PORT || 5432,
     server   = express(),
-    MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhoset:27017",
-    dbname   = "some_useful_name"
-    mongoose = require('mongoose');
+    MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+    dbname   = "some_useful_name",
+    mongoose = require('mongoose'),
+    ejs = require('ejs'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    expressEjsLayouts = require('express-ejs-layouts');
+
+//Initialize Packages
+server.set('views', ',/views');
+server.set('view engine','ejs');
+
+server.use(session({
+  secret: "caseypanzer",
+  resave: true,
+  saveUnitialized: false
+}));
+
+server.use(express.static('./public'));
+server.use(expressEjsLayouts);
+server.use(morgan('dev'));
+
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+server.use(methodOverride('_method'));
+
+//SERVER STUFF
 
 server.get('/test', function(req, res){
   res.write('Welcome to my amzing app');
