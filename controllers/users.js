0c180104c6f4e864ebdc248bee9c29ff2465b87b
Casey Panzer <caseypanzer@Caseys-Air.fios-router.home>
2015-10-12 13:28:16 -0400

    user login working

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,48 @@ var express = require('express'),
     User    = require('../models/user.js');
 
 // routes
+
+//SIGN UP
 router.get('/new', function(req, res){
   res.render('users/new');
+});
+
+router.post('/', function (req, res){
+  var newUser = User(req.body.user);
+  console.log(newUser);
+  newUser.save(function (err, user){
+    res.redirect(301,"/users/" + user._id)
+  });
+});
+
+//LOGIN
+router.get('/login', function (req, res){
+  res.render('users/login');
+});
+
+router.post('/login', function(req, res){
+  var attempt = req.body.user;
+
+  User.findOne({ email: attempt.email }, function(err, user) {
+    var message;
+
+    if( user && user.password == attempt.password ) {
+        req.session.currentUser = user.email;
+
+        res.redirect(302, '/users/welcome');
+    } else {
+        res.redirect(302, '/users/login');
+    };
+  });
+});
+
+//WELCOME
+router.get('/welcome', function(req, res){
+  if( req.session.currentUser ){
+    res.render('users/welcome');
+  }else{
+    res.redirect(302,'/users/login')
+  };
 });
 
 //export router object
